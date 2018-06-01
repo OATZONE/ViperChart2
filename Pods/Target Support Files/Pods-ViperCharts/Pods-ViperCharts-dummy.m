449f24bc80ef283338461955c290bc78252d76ba
Unknown <oatzone1@hotmail.com>
2018-06-01 17:18:19 +0700

    new

---
+++ Pods/Target Support Files/Pods-ViperCharts/Pods-ViperCharts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ViperCharts : NSObject
+@end
+@implementation PodsDummy_Pods_ViperCharts
+@end
---
+++ Pods/Target Support Files/Pods-ViperChartsUITests/Pods-ViperChartsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ViperChartsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ViperChartsUITests
+@end
